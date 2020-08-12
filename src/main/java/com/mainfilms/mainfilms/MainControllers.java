430e88c564b297f386b46835370db8b0d8bdd62b
vadman <vsam1998@mail.ru>
2020-08-12 17:36:18 +0500

    маппинг топ-100 и контроллера

--- src/main/java/com/mainfilms/mainfilms/MainControllers.java
+++ src/main/java/com/mainfilms/mainfilms/MainControllers.java
@@ -13,4 +13,11 @@ public class MainControllers {
         model.addAttribute("title", "Главная страница");
         return "mainPage";
     }
+
+    @GetMapping("/top-100")
+    public String top100(Model model) {
+        model.addAttribute("title", "Топ-100");
+        return "top-100";
+    }
+
 }
